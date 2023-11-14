b8a6fef688fde6ad60b96ac8ef93652b94eecb73
Hilcbek <balemayehu07@gmail.com>
2023-11-14 18:50:52 +0300

    adas

--- libs/Axios.js
+++ libs/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export let baseURL = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lastsdsd.onrender.com/api",
   withCredentials: true,
 });
 export let UploadImage = async (url) => {
@@ -14,4 +14,4 @@ export let UploadImage = async (url) => {
   );
   return res.data.secure_url;
 };
-//https://lastsdsd.onrender.com
+//
